5830a1b62ebdbb39b863bcfe7355e1c07b0c1ab3
koiinoyokannnn <bersekov.ajdamir@yandex.ru>
2026-03-28 14:21:32 +0300

    update

---
+++ src/main/java/com/bank/Event.java
@@ -0,0 +1,22 @@
+package com.bank;
+
+import java.util.Comparator;
+
+public class Event implements Comparable<Event>{
+    private EventType type;
+    private int duration;
+    private int startTime;
+    @Override
+    public int compareTo(Event o) {
+        // TODO Auto-generated method stub
+        return Integer.compare(this.startTime, o.startTime);
+    }
+}
+
+enum EventType {
+    ARRIVAL,
+    QUEUE_SELECTION,
+    STANDING,
+    SERVICE,
+    COMPLETING
+}
